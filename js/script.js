94a80ee762f61e3014766f073b5317a4a1d1844c
Алексей С <76011034+sem-a@users.noreply.github.com>
2024-02-05 11:57:22 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 const animStart = () => {
+    text1 = document.querySelector('.m1');
+    text2 = document.querySelector('.m2');
+    text3 = document.querySelector('.m3');
+    
+    text1.classList.add('_active_modal_text');
+    setTimeout(100, () => {
+        console.log('fsdf');
+        text1.classList.remove('_active_modal_text');
+    })
+
+}
+const animOneScreen = () => {
     discoBall = document.querySelector('.disco_ball_img');
     title = document.querySelector('.one_screen_title');
     text = document.querySelector('.one_screen_text');
